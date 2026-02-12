dd78da3990db6e29c76acbc3f6439c8c2440222d
ncastelli76 <ncastelli@wpi.edu>
2026-02-12 18:38:42 -0500

    task E complete

--- taskE.java
+++ taskE.java
@@ -3,6 +3,10 @@ import java.util.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -12,6 +16,9 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class taskE {
     // Custom Writable class to hold an array of IntWritable values (page and count)
     public static class IntArrayWritable extends ArrayWritable {
+    public IntArrayWritable() {
+        super(IntWritable.class);
+    }
 
     public IntArrayWritable(IntWritable[] values) {
         super(IntWritable.class, values);
@@ -19,7 +26,17 @@ public class taskE {
 
     @Override
     public IntWritable[] get() {
-        return (IntWritable[]) super.get();
+        Writable[] temp = super.get();
+        if (temp != null) {
+            int n = temp.length;
+            IntWritable[] items = new IntWritable[n];
+            for (int i = 0; i < temp.length; i++) {
+                items[i] = (IntWritable)temp[i];
+            }
+            return items;
+        } else {
+            return null;
+        }   
     }
 
     @Override
@@ -31,13 +48,14 @@ public class taskE {
 
 
     // Mapper class to map who accessed what page
-    public static class TaskEMapper extends Mapper<Text, Text, Text, IntArrayWritable> {
+    public static class TaskEMapper extends Mapper<LongWritable, Text, Text, IntArrayWritable> {
         private Text user = new Text();
         private IntWritable page = new IntWritable();
         //private IntWritable[] outValue = new IntWritable[2]; // to hold page and count of 1 for each access
 
-
-        public void map(Text key, IntArrayWritable value, Context context) throws IOException, InterruptedException {
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            context.getCounter("Mapper", "Input Records").increment(1);
             String[] fields = value.toString().split(",");
             if (fields.length >= 3) {
                 try {
@@ -45,11 +63,10 @@ public class taskE {
                     Integer pageId = Integer.parseInt(fields[1]);
                     user.set(userId);
                     //page.set(pageId);
-                    IntWritable[] temp = new IntWritable[2];
+                    IntWritable[] temp = new IntWritable[]{new IntWritable(pageId), new IntWritable(1)}; // create an array to hold page and count of 1 for each access
                     IntArrayWritable outValue = new IntArrayWritable(temp);
-                    temp[0].set(pageId);
-                    temp[1].set(1); // set count of 1 for each access
-                    outValue.set(temp);
+                    
+
                     context.write(user, outValue); // Emit (user, [page, 1]) for each page access
                 } catch (NumberFormatException e) {
                     // skip bad lines
@@ -59,11 +76,11 @@ public class taskE {
     }
 
     // Combiner class to sum up the counts for each page accessed by a user and count unique pages accessed
-    public static class TaskECombiner
-     extends Reducer<Object, IntArrayWritable, Text, IntArrayWritable> {
+    public static class TaskEReducer
+     extends Reducer<Text, IntArrayWritable, Text, IntArrayWritable> {
 
         //@Override
-        protected void reduce(Text key, Iterable<IntArrayWritable> values, Reducer<Text, IntArrayWritable, Text, IntArrayWritable>.Context context) throws IOException, InterruptedException {
+        protected void reduce(Text key, Iterable<IntArrayWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0; //for total # of accesses
             Set<IntWritable> uniquePages = new HashSet<>(); //to count unique pages accessed by the user
             // get the total count of page accesses for each page and count unique pages accessed by the user
@@ -73,14 +90,10 @@ public class taskE {
                 uniquePages.add(value[0]); // add page to the set of unique pages accessed
             }
             IntWritable accesses = new IntWritable(sum);
-            IntWritable[] temp = new IntWritable[2];
-            IntArrayWritable output = new IntArrayWritable(temp); // create an array to hold total accesses and unique pages accessed
-
-            temp[0].set(accesses.get());
-            temp[1].set(uniquePages.size());
+            IntWritable[] temp = new IntWritable[] {new IntWritable(accesses.get()), new IntWritable(uniquePages.size())}; // create an array to hold total accesses and unique pages accessed
             
-            output.set(temp);
-            context.write(key, new IntArrayWritable(output.get())); // Emit (user, total accesses, unique pages accessed)
+
+            context.write(key, new IntArrayWritable(temp)); // Emit (user, total accesses, unique pages accessed)
         }
     }
 
@@ -95,9 +108,11 @@ public class taskE {
         // 2. map the class
         job.setJarByClass(taskE.class);
 
+        //job.setInputFormatClass(TextInputFormat.class);
+
         // 3. both the mapper class and the reducer class
         job.setMapperClass(TaskEMapper.class);
-        job.setCombinerClass(TaskECombiner.class);
+        job.setReducerClass(TaskEReducer.class);
         job.setNumReduceTasks(1); // single reducer to compute global average
 
         // 4. set up the output key value data type class
@@ -115,7 +130,6 @@ public class taskE {
             new Path("file:///home/ds503/shared_folder/project1/taskE/output"));
 
         // 7. submit the job
-        boolean result = job.waitForCompletion(true);
 
         long startTime = System.nanoTime();
         boolean result = job.waitForCompletion(true);
